b322285a2f972214e50b2a1647fe101ad5ab329b
Teis Jayaswal <teisjayaswal@gmail.com>
2022-07-29 14:31:05 -0700

    Re-worked using class syntax plus module

--- library.js
+++ library.js
@@ -1,206 +1,130 @@
-let myLibrary = []; 
+class Book {
 
-function Book(title, author, pageCount, readStatus, numberBook) {
-    this.title = title
-    this.author = author
-    this.pageCount = pageCount
-    this.readStatus = readStatus
-    this.numberBook = numberBook 
+    constructor({title, author, pageCount, readStatus, numberBook}) {
+        this.title = title
+        this.author = author
+        this.pageCount = pageCount
+        this.readStatus = readStatus
+        this.numberBook = numberBook  
+    }
 }
 
+const library = (function(){
 
+    let myLibrary = []; 
 
-// const book1 = new Book('Zero to One', "Peter Thiel", "224 pages", "read", "1")
-// const book2 = new Book('You are a Badass at Making Money', "Jen Sincero", "211 pages", "reading", "2")
-
-// myLibrary.push(book1)
-// myLibrary.push(book2)
-// console.log(myLibrary);
-
-
-
-function openForm() {
-
-    var form = document.getElementById("myForm");
-    form.style.display = "block"; 
-
-    // form.addEventListener("submit", function() {
-        
-        // return false
+    const openForm = () => {
+        var form = document.getElementById("myForm");
+        form.style.display = "block"; 
     }
 
-function addBooktoLibrary() {
+    const addBooktoLibrary  = () => {
+        var form = document.getElementById("myForm");
+        let newBook = new Book({});
 
-    var form = document.getElementById("myForm");
-    var newBook = new Book();
+        newBook.title = document.getElementById("title").value;
+        newBook.author = document.getElementById("author").value || 'asldjfalsdjf';
+        newBook.pageCount = document.getElementById("page-count").value || 8;
+        newBook.readStatus = document.querySelector('input[name="readstatus"]:checked')?.value || "yes";  
+        newBook.numberBook = myLibrary.length + 1;
 
-    newBook.title = document.getElementById("title").value;
-    newBook.author = document.getElementById("author").value || 'asldjfalsdjf';
-    newBook.pageCount = document.getElementById("page-count").value || 8;
-    newBook.readStatus = document.querySelector('input[name="readstatus"]:checked')?.value || "yes";  
-    newBook.numberBook = myLibrary.length + 1;
+        myLibrary.push(newBook);
+        console.log(myLibrary);
+        
+        const inputs = document.querySelectorAll('#title, #author, #page-count');
 
-    myLibrary.push(newBook);
-    console.log(myLibrary);
-    
-    const inputs = document.querySelectorAll('#title, #author, #page-count');
-
-    inputs.forEach(input => {
-        input.value = '';
-  });
-
-    closeForm();
-    displayCard();
-} 
-
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
-
-function displayCard() {
-
-    const booksToReset = document.querySelectorAll('.bookClass');
-
-    booksToReset.forEach(book => {
-        book.remove(); 
-    });
-
-    var bookContainer = document.getElementById("book-container"); 
-
-    myLibrary.forEach (book => {
-        const card = document.createElement('div');
-        card.classList.add('bookClass') 
-        const title = document.createElement('h3'); 
-        title.textContent = `Title: ${book.title}`
-        const author = document.createElement('h3');
-        author.textContent = `Author: ${book.author}`
-        const pageCount = document.createElement('h3');
-        pageCount.textContent = `Number of Pages: ${book.pageCount}`;
-        const readStatus = document.createElement('h3');
-        readStatus.textContent = `Read Status: ${book.readStatus}`;
-        const removeButton = document.createElement('button');
-        removeButton.classList.add('removeCard');
-        removeButton.setAttribute('id', `${myLibrary.indexOf(book)}`);
-        removeButton.textContent = "Remove Book"; 
-        const readButton = document.createElement('button');
-        readButton.classList.add('readStatusChange');
-        readButton.textContent = "Change Read Status";
-        readButton.setAttribute('id', `${myLibrary.indexOf(book)}`);
-
-        card.appendChild(title);
-        card.appendChild(author); 
-        card.appendChild(pageCount);
-        card.appendChild(readStatus); 
-        card.appendChild(removeButton); 
-        card.appendChild(readButton);
-        bookContainer.appendChild(card);
-
-
-    })
-    console.log(`About to set up the remove button handlers`)
-    setupRemoveButtonCallbacks();
-    console.log(`Just set up the remove button handlers`)
-    console.log(`About to set up edit read status button handlers`)
-    setupEditReadStatus(); 
-    console.log(`Just set up edit read status button handlers`)
-
-}
-
-function setupEditReadStatus() {
-    const editReadStatusButtons = document.querySelectorAll('.readStatusChange');
-
-    Array.from(editReadStatusButtons).forEach((editReadStatusButton) => {
-        console.log(`Setting up listner for readStatusButton with id ${editReadStatusButton.id}`);
-        editReadStatusButton.addEventListener('click', function(event) {
-            if (myLibrary[editReadStatusButton.id].readStatus === "yes") {
-                myLibrary[editReadStatusButton.id].readStatus = "no";
-            } else if (myLibrary[editReadStatusButton.id].readStatus === "no") {
-                myLibrary[editReadStatusButton.id].readStatus = "yes"; 
-            }; 
-            console.log(myLibrary[editReadStatusButton.id].readStatus)
-            displayCard();
+        inputs.forEach(input => {
+            input.value = '';
         });
-    });
 
+        closeForm();
+        displayCard();
+    }
 
-}
+    const closeForm = () => {
+        document.getElementById("myForm").style.display = "none";
+    }
 
-// ```
-// Hypothesis:
+    const displayCard = () => {
+        const booksToReset = document.querySelectorAll('.bookClass');
 
-// ```
-
-function setupRemoveButtonCallbacks() {
-
-    const removeButtons = document.querySelectorAll('.removeCard');
-
-    Array.from(removeButtons).forEach((removeButton) => {
-        console.log(`Setting up listener for removeButton with id ${removeButton.id}`);
-        // removeButton.addEventListener('click', function(event) {
-        //     event.stopImmediatePropagation()
-        //     console.log("you clicked it and now we're here")
-        // })
-        // removeButton.addEventListener('click', function(event) {
-        //     console.log("you clicked it and now we're ALSOSOSOSDOSOSOS here")
-        // })
-        removeButton.addEventListener('click', function(event) {
-            // event.stopImmediatePropagation();
-            console.log(removeButton.parentNode);
-            console.log(`Inside event listener callback for id ${removeButton.id}`)
-            // removeButton.parentNode.remove();
-            console.log(removeButton.id);
-            myLibrary.splice(removeButton.id, 1);
-            console.log(myLibrary); 
-            displayCard(); 
-            
-            // for (let i = 0; i < myLibrary.length; i++) {
-            //     if (removeButton.classList.contains(i)) {
-            //         myLibrary.splice(i, 1)
-            //         console.log(myLibrary
-            //     };
-            // };
-           
-            
+        booksToReset.forEach(book => {
+            book.remove(); 
         });
-    }); 
-}
 
-// function handleClickEvent(event) {
-    
-// }
+        var bookContainer = document.getElementById("book-container"); 
 
+        myLibrary.forEach (book => {
+            const card = document.createElement('div');
+            card.classList.add('bookClass') 
+            const title = document.createElement('h3'); 
+            title.textContent = `Title: ${book.title}`
+            const author = document.createElement('h3');
+            author.textContent = `Author: ${book.author}`
+            const pageCount = document.createElement('h3');
+            pageCount.textContent = `Number of Pages: ${book.pageCount}`;
+            const readStatus = document.createElement('h3');
+            readStatus.textContent = `Read Status: ${book.readStatus}`;
+            const removeButton = document.createElement('button');
+            removeButton.classList.add('removeCard');
+            removeButton.setAttribute('id', `${myLibrary.indexOf(book)}`);
+            removeButton.textContent = "Remove Book"; 
+            const readButton = document.createElement('button');
+            readButton.classList.add('readStatusChange');
+            readButton.textContent = "Change Read Status";
+            readButton.setAttribute('id', `${myLibrary.indexOf(book)}`);
 
-// var prevent = function() {
-//     var form = document.getElementById("myForm").addEventListener("submit", function(event) {
-//         event.preventDefault();
-//     })
-// }
+            card.appendChild(title);
+            card.appendChild(author); 
+            card.appendChild(pageCount);
+            card.appendChild(readStatus); 
+            card.appendChild(removeButton); 
+            card.appendChild(readButton);
+            bookContainer.appendChild(card);
+        })
+            console.log(`About to set up the remove button handlers`)
+            setupRemoveButtonCallbacks();
+            console.log(`Just set up the remove button handlers`)
+            console.log(`About to set up edit read status button handlers`)
+            setupEditReadStatus(); 
+            console.log(`Just set up edit read status button handlers`)
+    }
 
-// prevent(); 
+    const setupEditReadStatus = () => {
 
-// var form = document.getElementById("myForm").addEventListener("submit", function() {
+        const editReadStatusButtons = document.querySelectorAll('.readStatusChange');
 
-//     var newBook = new Book();
+        Array.from(editReadStatusButtons).forEach((editReadStatusButton) => {
+            console.log(`Setting up listner for readStatusButton with id ${editReadStatusButton.id}`);
+            editReadStatusButton.addEventListener('click', function(event) {
+                if (myLibrary[editReadStatusButton.id].readStatus === "yes") {
+                    myLibrary[editReadStatusButton.id].readStatus = "no";
+                } else if (myLibrary[editReadStatusButton.id].readStatus === "no") {
+                    myLibrary[editReadStatusButton.id].readStatus = "yes"; 
+                }; 
+                console.log(myLibrary[editReadStatusButton.id].readStatus)
+                displayCard();
+            });
+        });
+    }
 
-//     newBook.title = document.getElementById("title").value;
-//     newBook.author = document.getElementById("author").value;
-//     newBook.pageCount = document.getElementById("page-count").value;
-//     newBook.readStatus = document.getElementsByName("readstatus").value;
+    const setupRemoveButtonCallbacks = () => {
+        const removeButtons = document.querySelectorAll('.removeCard');
 
-//     myLibrary.push(newBook)
+        Array.from(removeButtons).forEach((removeButton) => {
+            console.log(`Setting up listener for removeButton with id ${removeButton.id}`);
+            removeButton.addEventListener('click', function(event) {
+                console.log(removeButton.parentNode);
+                console.log(`Inside event listener callback for id ${removeButton.id}`)
+                console.log(removeButton.id);
+                myLibrary.splice(removeButton.id, 1);
+                console.log(myLibrary); 
+                displayCard();
+                
+            });
+        });
+    }
 
-// })
+    return {openForm, addBooktoLibrary, closeForm, displayCard, setupEditReadStatus, setupRemoveButtonCallbacks}
 
-
-    // form.onsubmit = function() {
-
-    //     title = document.getElementById("title").value
-    //     author = document.getElementById("author").value
-    //     pageCount = document.getElementById("page-count").value
-    //     readStatus =document.getElementsByName("readstatus").value
-
-    //     newBook = new Book(title, author, pageCount, readStatus)
-    //     myLibrary.push(newBook)
-    // }
-
-    // const container = document.querySelector('.book-titles')
+})();
